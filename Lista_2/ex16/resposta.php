23c3621b46958112cbd0b1c13f95244989de6e65
Jezidi990 <jpizidiogomes@gmail.com>
2024-09-25 19:18:24 -0300

    lista 2  concluida

--- Lista_2/ex16/resposta.php
+++ Lista_2/ex16/resposta.php
@@ -3,25 +3,28 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>resp</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta do Exercício 1</h1>
-    <?php 
-            if($_SERVER["REQUEST_METHOD"] == 'POST'){
-                try { 
-                    $valor1 = $_POST['valor1'];
-                    $valor2 = $_POST['valor2'];
-                // + - * / % ++ -- ** operadores aritméticos
-                $resultado = $valor1 + $valor2;
-                echo "<p>Soma: $resultado </p>"; }
-                 catch(Exception $e){
-                    echo "Erro!".$e->getMessage();
-                }
-            }
+    <main class="container">
+        <?php 
+                if($_SERVER["REQUEST_METHOD"]=="POST")
+                {
+                    try{
+                        (int)$n1 = $_POST["preco"];
+                        (int)$n2 = $_POST["desconto"];
 
-    ?>
+                        (int)$resposta = $n1 - $n1 * ($n2 / 100);
+
+                        echo "<p>O preço final é: $resposta </p>";
+                        }        
+                    catch (Exception $e)
+                        {echo "Erro: " .$e -> getMessage();}
+                }
+                ?>
+
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
--- Lista_2/ex17/resposta.php
+++ Lista_2/ex17/resposta.php
@@ -7,21 +7,23 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta do Exercício 1</h1>
-    <?php 
-            if($_SERVER["REQUEST_METHOD"] == 'POST'){
-                try { 
-                    $valor1 = $_POST['valor1'];
-                    $valor2 = $_POST['valor2'];
-                // + - * / % ++ -- ** operadores aritméticos
-                $resultado = $valor1 + $valor2;
-                echo "<p>Soma: $resultado </p>"; }
-                 catch(Exception $e){
-                    echo "Erro!".$e->getMessage();
-                }
-            }
+    <main class="container">
+        <?php 
+                if($_SERVER["REQUEST_METHOD"]=="POST"){
+                    try{
+                        (float)$n1 = $_POST["n1"];
+                        (float)$n2 = $_POST["n2"];
+                        (float)$n3 = $_POST["n3"];
+                        (float)$totaljuros = $n1 * $n2 * $n3;
 
-    ?>
+                        echo "<p> O total em juros é: $totaljuros </p>";
+                    }
+                
+                    catch (Exception $e)
+                        {echo "Erro: " .$e -> getMessage();}
+                }
+        ?>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
-</html>
+</html> 
