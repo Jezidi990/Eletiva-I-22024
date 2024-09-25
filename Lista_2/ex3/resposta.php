602dae3b9b4c163cb23eae0d07be6a9b9bdd2170
Jezidi990 <jpizidiogomes@gmail.com>
2024-09-24 21:35:37 -0300

    ex6 completo

--- Lista_2/ex3/resposta.php
+++ Lista_2/ex3/resposta.php
@@ -3,15 +3,28 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>resp</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-   <main class="container">
-    <form action="Resposta.php" method="post">
-        
-    </form>
-   </main>
+    <main class="container">
+        <?php 
+                if($_SERVER["REQUEST_METHOD"]=="POST")
+                {
+                    try{
+                        (int)$n1 = $_POST["n1"];
+                        (int)$n2 = $_POST["n2"];
+
+                        (int)$resposta = $n1 * $n2;
+
+                        echo "<p>Resultado: $resposta </p>";
+                        }        
+                    catch (Exception $e)
+                        {echo "Erro: " .$e -> getMessage();}
+                }
+                ?>
+
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
---
+++ Lista_2/ex6/resposta.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <main class="container">
+        <?php 
+                if($_SERVER["REQUEST_METHOD"]=="POST"){
+                    try{
+                        (float)$temp_celsius = $_POST["temperatura"];
+
+                        (float)$temp_faren = ($temp_celsius * 1.8) + 32;
+                        
+                        echo "<p> a temperatura em farenheit é: $temp_faren</p>";
+                        
+                    }
+                
+                    catch (Exception $e)
+                        {echo "Erro: " .$e -> getMessage();}
+                }
+        ?>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html> 
